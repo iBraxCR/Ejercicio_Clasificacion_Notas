a2ccfaa237e4d111b26f8f48c2f1922db3c291af
iBrax <119822950+iBraxCR@users.noreply.github.com>
2023-10-15 17:27:43 -0600

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,4 +1,9 @@
-
+/*
+ * 
+ * Ejercicio: Pedir una nota de 0 al 100 y mostrarla de la forma insuficiente, Suficiente, Bien, Notable y 
+ * sobresaliente
+ * 
+ */
 
 
 import javax.swing.JOptionPane;
@@ -6,12 +11,15 @@ import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        // Declaración de la variable para almacenar la nota
         int nota;
 
+        // Solicitar al usuario que ingrese la nota
         nota = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite la nota"));
 
+        // Comprobar la nota y mostrar un mensaje correspondiente
         if (nota >= 90 && nota <= 100) {
-            JOptionPane.showMessageDialog(null, "Nota Excepcional");
+            JOptionPane.showMessageDialog(null, "Nota Sobresaliente");
         } else if (nota >= 80) {
             JOptionPane.showMessageDialog(null, "Nota Notable");
         } else if (nota >= 70) {
@@ -21,6 +29,7 @@ public class App {
         } else if (nota < 60) {
             JOptionPane.showMessageDialog(null, "Nota Insuficiente");
         } else {
+            // Si la nota no se ajusta a ninguno de los rangos anteriores, se considera inválida
             JOptionPane.showMessageDialog(null, "Nota inválida");
         }
     }
